d1e46e5df86ecedc2e962ec3c2d5deb1e9947d81
michaelnyirenda <michael00nyirenda@gmail.com>
2025-03-29 23:56:49 +0200

    Add Card data class with identifier and list of words

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -4,9 +4,39 @@ import org.springframework.stereotype.Service
 
 @Service
 class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
+    val identifierFactory: IdentifierFactory = IdentifierFactory()
+    val cards: List<Card> = generateCards()
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+    companion object {
+        const val WORDS_IN_CARD = 4
+        val words: List<String> = listOf(
+            "apple", "banana", "car", "dog", "elephant", "flower", "guitar", "house",
+            "ice cream", "jacket", "kite", "laptop", "moon", "notebook", "orange",
+            "pencil", "queen", "rabbit", "sun", "tree", "umbrella", "violin", "water",
+            "xylophone", "yacht", "zebra", "airplane", "book", "cat", "dolphin",
+            "eagle", "fish", "giraffe", "hat", "island", "jellyfish", "koala"
+            // You can add more words if needed
+        )
+    }
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+    fun List<String>.toWords(): List<Word> = this.map { Word(it) }
+
+    private fun generateCards(cardsAmount: Int = 10): List<Card> {
+        val shuffledWords = words.shuffled()
+        return shuffledWords
+            .chunked(WORDS_IN_CARD)
+            .take(cardsAmount)
+            .map { chunk ->
+                Card(
+                    id = identifierFactory.uniqueIdentifier(),
+                    words = chunk.toWords()
+                )
+            }
+    }
+
+    fun getCardByIndex(index: Int): Card {
+        return cards.getOrElse(index) {
+            throw IllegalArgumentException("Card with index $index does not exist")
+        }
+    }
 }

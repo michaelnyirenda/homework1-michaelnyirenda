83e4cf964c4bd470702da2f797e722504d58c303
michaelnyirenda <michael00nyirenda@gmail.com>
2025-03-29 23:45:12 +0200

    Add Team data class with unique identifier and points

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamModel.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamModel.kt
@@ -1,9 +1,1 @@
 package jetbrains.kotlin.course.alias.team
-
-//Team Data Class
-
-import jetbrains.kotlin.course.alias.util.Identifier
-
-data class Team(val id: Identifier, var points: Int = 0) {
-    val name = "Team#${id + 1}"
-}

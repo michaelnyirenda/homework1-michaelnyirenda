29897ae466ac68959e5998ee385f495e222360e2
michaelnyirenda <michael00nyirenda@gmail.com>
2025-03-30 22:31:36 +0200

    TeamService update

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,22 +1,33 @@
 package jetbrains.kotlin.course.alias.team
 
 import org.springframework.stereotype.Service
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 
 @Service
 class TeamService {
-    val identifierFactory: IdentifierFactory = IdentifierFactory()
+    // property to generate identifiers for teams
+    val identifierFactory = IdentifierFactory()
 
     companion object {
+        // storage for all teams
         val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
     }
 
     fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
-        val teams = List(teamsNumber) {
+        val teams = mutableListOf<Team>()
+
+        repeat(teamsNumber) {
+            // generate a new unique id using the identifierFactory
             val id = identifierFactory.uniqueIdentifier()
-            Team(id)
+            // create a new team with the generated id
+            val team = Team(id)
+            // add the team to our list
+            teams.add(team)
+            // store the team in the teamsStorage map
+            teamsStorage[id] = team
         }
-        teams.forEach { team -> teamsStorage[team.id] = team }
+
         return teams
     }
-
 }

f16a832fab892dc07b180701894fb761a19cf513
michaelnyirenda <michael00nyirenda@gmail.com>
2025-03-30 22:38:09 +0200

    Refactor CardService to improve card generation and error handling

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,42 +1,41 @@
 package jetbrains.kotlin.course.alias.card
 
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
 @Service
 class CardService {
-    val identifierFactory: IdentifierFactory = IdentifierFactory()
+    // generate unique card identifiers
+    val identifierFactory = IdentifierFactory()
+
+    // store all cards
     val cards: List<Card> = generateCards()
 
     companion object {
+        // number of words per card
         const val WORDS_IN_CARD = 4
-        val words: List<String> = listOf(
-            "apple", "banana", "car", "dog", "elephant", "flower", "guitar", "house",
-            "ice cream", "jacket", "kite", "laptop", "moon", "notebook", "orange",
-            "pencil", "queen", "rabbit", "sun", "tree", "umbrella", "violin", "water",
-            "xylophone", "yacht", "zebra", "airplane", "book", "cat", "dolphin",
-            "eagle", "fish", "giraffe", "hat", "island", "jellyfish", "koala"
-            // You can add more words if needed
-        )
+
+        // words list
+        val cardsAmount = words.size / WORDS_IN_CARD
     }
 
-    fun List<String>.toWords(): List<Word> = this.map { Word(it) }
+    // convert a list of strings to a list of words
+    private fun List<String>.toWords(): List<Word> = this.map { Word(it) }
 
-    private fun generateCards(cardsAmount: Int = 10): List<Card> {
+    // generate all cards by shuffling words
+    private fun generateCards(): List<Card> {
         val shuffledWords = words.shuffled()
-        return shuffledWords
-            .chunked(WORDS_IN_CARD)
+        return shuffledWords.chunked(WORDS_IN_CARD)
             .take(cardsAmount)
             .map { chunk ->
-                Card(
-                    id = identifierFactory.uniqueIdentifier(),
-                    words = chunk.toWords()
-                )
+                Card(identifierFactory.uniqueIdentifier(), chunk.toWords())
             }
     }
 
+    // get a card by its index or throw an error if it doesn't exist
     fun getCardByIndex(index: Int): Card {
         return cards.getOrElse(index) {
-            throw IllegalArgumentException("Card with index $index does not exist")
+            throw Error("Card with index $index does not exist")
         }
     }
 }

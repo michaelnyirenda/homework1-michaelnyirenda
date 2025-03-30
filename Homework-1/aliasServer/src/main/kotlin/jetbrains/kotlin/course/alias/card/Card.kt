8beff3596b152b2b8495eea897b2e9a4b5fda0c1
michaelnyirenda <michael00nyirenda@gmail.com>
2025-03-30 22:35:52 +0200

    Refactor card management by removing old Card and Word classes and implementing GameStateManager for game state persistence

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/Card.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.kotlin.course.alias.card
-
-import jetbrains.kotlin.course.alias.util.Identifier
-
-data class Card(
-    val id: Identifier,
-    val words: List<Word>
-)
--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/Word.kt
+++
@@ -1,4 +0,0 @@
-package jetbrains.kotlin.course.alias.card
-
-@JvmInline
-value class Word(val word: String)
